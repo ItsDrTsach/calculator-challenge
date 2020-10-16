57114916f78feb64ccb525f4d746316574d48408
tsach ovadia <67857438+ItsDrTsach@users.noreply.github.com>
2020-10-16 22:15:19 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import Calc from './components/Calc';
+import React from "react";
+import Calc from "./components/Calc";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className="App">
+      <header className="App-header">
         <code> Write the functions that will make me work!</code>
       </header>
       <Calc />
     </div>
   );
 }
-
+// added that comment to triger action
 export default App;
